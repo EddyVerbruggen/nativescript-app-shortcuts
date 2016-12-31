ac47583ae49d3971d474da813383231ea2a22e7e
Eddy Verbruggen <eddyverbruggen@gmail.com>
2016-12-31 11:27:01 +0100

    cleanup

--- demo/app/beer-page.ts
+++ demo/app/beer-page.ts
@@ -1,5 +1,4 @@
 import * as observable from "data/observable";
-import * as pages from "ui/page";
 
 // Event handler for Page "loaded" event attached in beer-page.xml
 export function pageLoaded(args: observable.EventData) {
