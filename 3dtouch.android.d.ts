afede98cbccb697bcd5e4ba64e958a65301088af
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-08-28 14:01:39 +0200

    cleanup

--- 3dtouch.android.d.ts
+++
@@ -1,6 +0,0 @@
-import { LaunchQuickAction } from "nativescript-3dtouch";
-export declare class ThreeDeeTouch {
-    available(avail: boolean): Promise<boolean>;
-    configureQuickActions(actions: Array<any>): Promise<any>;
-    setQuickActionCallback(callback: (data: LaunchQuickAction) => void): void;
-}
